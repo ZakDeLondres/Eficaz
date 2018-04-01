f9591a118fe77b3210683bd11ceda1086e0beb23
Zakariyah Abdel-Illah <abs.zakariyah@gmail.com>
2018-04-01 18:49:07 +0100

    mysql: insert/create placeholders

--- db/mysql.c
+++ db/mysql.c
@@ -27,3 +27,58 @@ int db_create_datasource(MYSQL **db, char *name) {
 
     mysql_query(_db, query);
 }
+
+int db_create_datatable(MYSQL **db, char *name, db_table_column **columns) {
+    MYSQL *_db = *db;
+    char *query = malloc(sizeof(char) * (strlen("CREATE TABLE ()") + strlen(name)));
+    strcpy(query, "CREATE TABLE ");
+    strcat(query, name);
+    strcat(query, "(");
+
+//    while (*columns++) {
+//        db_table_column *column = *columns;
+//        query = realloc(query, sizeof(query) + (sizeof(char) * (strlen(column->name) + strlen(" ") + strlen(column->db_value_type))));
+//    }
+
+    strcat(query, ")");
+
+    mysql_query(_db, query);
+}
+
+int db_post_data(MYSQL **db, char *target, db_post_value **value) {
+    MYSQL *_db = *db;
+    char *query = malloc(sizeof(char) * (strlen("INSERT INTO VALUES()") + strlen(target)));
+    strcpy(query, "INSERT INTO ");
+    strcat(query, target);
+    strcat(query, " VALUES(");
+
+//    while (*columns++) {
+//        db_table_column *column = *columns;
+//        query = realloc(query, sizeof(query) + (sizeof(char) * (strlen(column->name) + strlen(" ") + strlen(column->db_value_type))));
+//    }
+
+    strcat(query, ")");
+
+    mysql_query(_db, query);
+}
+
+db_recv_row **db_recv_data(MYSQL **db, char *target) {
+    MYSQL *_db = *db;
+    char *query = malloc(sizeof(char) * (strlen("SELECT * FROM ") + strlen(target)));
+    strcpy(query, "SELECT * FROM ");
+    strcat(query, target);
+
+    mysql_query(_db, query);
+
+    MYSQL_RES *result = mysql_store_result(_db);
+    int MYSQL_FIELD_COUNT = mysql_num_fields(_db);
+    MYSQL_ROW *row;
+
+    while ((row == mysql_fetch_row(result))) {
+        for (int i = 0; i < MYSQL_FIELD_COUNT, i++) {
+            printf("RECV");
+        }
+    }
+
+    mysql_free_result(result);
+}
