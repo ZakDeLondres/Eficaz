f853e16e951546f3fb83015d425f7f5bcb0c3ce5
Zakariyah Abdel-Illah <abs.zakariyah@gmail.com>
2018-04-02 00:03:25 +0100

    eficaz_context: populate create_context() with test

--- env/context.c
+++ env/context.c
@@ -7,25 +7,21 @@
 #include "context.h"
 
 eficaz_context ** create_context(eficaz_project *project, eficaz_sequence *sequence, eficaz_shot *shot) {
+    eficaz_context *_context = malloc(sizeof(eficaz_context) + 1);
+    eficaz_project *_project = malloc(sizeof(eficaz_project) + 1);
+    eficaz_sequence *_seq = malloc(sizeof(eficaz_sequence) + 1);
+    eficaz_shot *_shot = malloc(sizeof(eficaz_shot) + 1);
 
+    _project->name = malloc(sizeof(char) * strlen("TESTPROJECT"));
+    strcpy(_project->name, "TESTPROJECT");
+
+    _context->project = _project;
+    _context->sequence = _seq;
+    _context->shot = _shot;
+
+    return &_context;
 }
 
 eficaz_context ** update_context(eficaz_context **context) {
 
-}
-
-eficaz_context ** create_test_context() {
-    eficaz_context *context = malloc(sizeof(eficaz_context) + 1);
-    eficaz_project *project = malloc(sizeof(eficaz_project) + 1);
-    eficaz_sequence *seq = malloc(sizeof(eficaz_sequence) + 1);
-    eficaz_shot *shot = malloc(sizeof(eficaz_shot) + 1);
-
-    project->name = malloc(sizeof(char) * strlen("TESTPROJECT"));
-    strcpy(project->name, "TESTPROJECT");
-
-    context->project = project;
-    context->sequence = seq;
-    context->shot = shot;
-
-    return &context;
 }
