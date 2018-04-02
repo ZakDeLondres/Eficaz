ce073cc373345a42fc18a70d57d131cb79022847
Zakariyah Abdel-Illah <abs.zakariyah@gmail.com>
2018-04-02 01:58:51 +0100

    env/project: loading of all projects from database

--- env/project.c
+++ env/project.c
@@ -2,4 +2,23 @@
 // Created by zakdelondres on 03/03/18.
 //
 
+#include <printf.h>
+#include <stdlib.h>
+#include <memory.h>
 #include "project.h"
+
+eficaz_project ** create_projects_from_db(EFICAZ_DB_PROTOCOL **db) {
+    db_recv_rows *data = *db_recv_data(&db, "eficaz_projects");
+    eficaz_project **projects = malloc(sizeof(eficaz_project *) * data->count);
+
+    for (int i = 0; i < data->count; i++) {
+        projects[i] = malloc(sizeof(eficaz_project));
+        projects[i]->name = malloc(sizeof(data->rows[i]->data[2]));
+        projects[i]->code = malloc(sizeof(data->rows[i]->data[1]));
+
+        strcpy(projects[i]->name, data->rows[i]->data[2]);
+        strcpy(projects[i]->code, data->rows[i]->data[1]);
+    }
+
+    return projects;
+}
--- env/project.h
+++ env/project.h
@@ -5,9 +5,13 @@
 #ifndef LIBEFICAZ_PROJECT_H
 #define LIBEFICAZ_PROJECT_H
 
+#include "../db/def.h"
+
 struct libeficaz_project {
     char **code;
     char **name;
 } typedef eficaz_project;
+
+eficaz_project ** create_projects_from_db(EFICAZ_DB_PROTOCOL **db);
 
 #endif //LIBEFICAZ_PROJECT_H
